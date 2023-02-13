fe6a45c9c27df431e76d0190637068b819b28a67
Alok Pandey <62480319+alokp2@users.noreply.github.com>
2023-02-13 15:38:01 +0530

    Add files via upload

---
+++ Pythoncourse/classwork/cw1/swappingnumber.py
@@ -0,0 +1,11 @@
+a=int(input("Enter the value of a: "))
+b=int(input("Enter the value of b: "))
+
+print("Before swapping  a=",a,"b=",b);
+
+t=a;
+a=b;
+b=t;
+
+print("After swapping a and b")
+print("a=",a,"b=",b)

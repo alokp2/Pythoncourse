66fbe4eb7a3be8a227fad83fb4664a1d8b4129d3
Alok Pandey <62480319+alokp2@users.noreply.github.com>
2023-02-13 15:39:14 +0530

    Add files via upload

---
+++ classwork/cw1/arithmetic.py
@@ -0,0 +1,9 @@
+n1 = 54
+n2 = 47
+print("**************************************************************")
+print("Addition : ",(n1+n2))
+print("Subtraction : ",(n1-n2))
+print("Division : ",(n1/n2))
+print("Multiplication :",(n1*n2))
+print("*"*60)
+

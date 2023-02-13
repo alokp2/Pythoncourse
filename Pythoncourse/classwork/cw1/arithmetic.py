8afed4ebeefb077a0b5e7a9adeeeb2b537d0ff9e
Alok Pandey <62480319+alokp2@users.noreply.github.com>
2023-02-13 15:38:50 +0530

    Delete Pythoncourse/classwork/cw1 directory

--- Pythoncourse/classwork/cw1/arithmetic.py
+++
@@ -1,9 +0,0 @@
-n1 = 54
-n2 = 47
-print("**************************************************************")
-print("Addition : ",(n1+n2))
-print("Subtraction : ",(n1-n2))
-print("Division : ",(n1/n2))
-print("Multiplication :",(n1*n2))
-print("*"*60)
-

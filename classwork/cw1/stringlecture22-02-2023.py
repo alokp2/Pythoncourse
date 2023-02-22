957a0446fbdfc9b4aed525147f3e6dc81998df04
Alok Pandey <62480319+alokp2@users.noreply.github.com>
2023-02-22 10:02:30 +0530

    Add files via upload

---
+++ classwork/cw1/stringlecture22-02-2023.py
@@ -0,0 +1,34 @@
+str ="I am learning python"
+print(str)
+print(str.upper())
+print(str.lower())
+print(str.capitalize())
+print(str.casefold())
+print(str.center(12))
+print("python".isupper())
+print("python".replace("t","w"))
+print(str.find("learning"))
+
+#Slices
+
+s = "I am learning python"
+print(s[:-1])
+print(s[1:-1])
+print(s[2:5])
+print(s[-1:-5:-2])
+print(s[3:])
+print(s[1:8:2])
+print(s[0:20])
+print(s[5:14])
+print(s[-8:])
+
+
+
+
+
+
+
+
+
+
+
